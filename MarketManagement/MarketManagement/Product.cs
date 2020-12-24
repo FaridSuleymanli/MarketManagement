1f22dfb89865bcf54fb98413666fb3869540de59
Farid <faridms@code.edu.az>
2020-12-25 02:16:34 +0400

    Final commit

--- MarketManagement/MarketManagement/Product.cs
+++ MarketManagement/MarketManagement/Product.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace MarketIdareEtmeSistemi
 {
-    public enum Category { Meyve_Terevez = 1, Ickiler, Sirniyyat, Et_Mehsullari, Un_Memulatlari }
+    public enum Category { Meyve_Terevez = 1, Ickiler, Sirniyyat, Et_Mehsullari, Un_Memulatlari } //Kategoriya yaradiriq enumdan
     class Product
     {
         public string Name { get; set; }
         public double Price { get; set; }
-        public Category Categ { get; set; }
+        public Category Categ { get; set; } //enumdan instance alib, property yaradiriq
         public int Count { get; set; }
         public int Code { get; set; }
 
         public Product(int code, string name, double price, int count, Category category)
-        {
+        {   //product constructor yaradib, parametrleri elave edirik
             Name = name;
             Price = price;
             Count = count;
@@ -24,7 +24,7 @@ namespace MarketIdareEtmeSistemi
             Categ = category;
         }
         public override string ToString()
-        {
+        {   //ekrana cixacaq deyerleri stringe ceviririk
             return $"{Code} {Name} {Price} {Count} {Categ}";
         }
     }
