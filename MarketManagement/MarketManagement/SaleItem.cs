68c907619cbff864b65db03c0c46ae8a2186beef
Farid <faridms@code.edu.az>
2020-12-25 01:54:29 +0400

    Final commit

--- MarketManagement/MarketManagement/SaleItem.cs
+++ MarketManagement/MarketManagement/SaleItem.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace MarketIdareEtmeSistemi
 {
-    class SaleItem
+    class SaleItem //Satis Item klasi yaradiriq
     {
-        public Product Product { get; set; }
-        public Sale Sale { get; set; }
-        private static int _id = 0;
+        public Product Product { get; set; } //Product klasindan property yaradiriq
+        public Sale Sale { get; set; } //Sale klasindan property yaradiriq
+        private static int _id = 0; 
         public readonly int Id;
         public int Count { get; set; }
 
-        public SaleItem()
+        public SaleItem() //bos constructor yaradib private id-ni set edirik
         {
             _id++;
             Id = _id;
-        }
-        public SaleItem(Product product, int count) : this()
+        }  //yeni bir constructor yaradib, parametrlerini veririk
+        public SaleItem(Product product, int count) : this() //bos constructoru isletmek ucun
         {
             Product = product;
             Count = count;
         }
         public override string ToString()
-        {
+        {   //ekrana cixacaq deyerleri stringe ceviririk
             return $"{Id} {Product.Name} {Count}";
         }
 
